da424dec8aae542d887427601398e0a7a6d7f3a4
self067 <seltor@mail.ru>
2021-01-31 00:48:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
+// temporary
+  if( number > 1000) return 'too many';
+  if( number === 1000) return 'one thousand';
+  if( number === 0) return 'zero';
+
+  const dig1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const dig11 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',  'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dig10 = ['','', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  // one 'hundred'
+  // 
+let res = '';
+const ln = (''+number).length;
+
+  (''+number).split('').forEach((v,i,ar)=>{
+    const vnum = parseInt(v);
+    if( ln-i === 4 ) res += dig1[vnum] + ' thousand';
+    if(ln-i === 3 && vnum ) res += dig1[vnum] + ' hundred';
+    if(ln-i === 2 && vnum > 1 ) res += ' '+dig10[vnum];
+
+    if(ln-i === 1) {
+      res += ' ';
+      if( ln > 1 && ar[i-1] === '1') res += dig11[vnum];
+      else res += dig1[vnum];
+    }
+
+  });
+
+  return res.trim();
 }
